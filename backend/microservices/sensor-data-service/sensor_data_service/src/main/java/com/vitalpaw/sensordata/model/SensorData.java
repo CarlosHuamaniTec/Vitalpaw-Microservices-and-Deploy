547bffa10026dda55bade138f2e15b97ff77d359
@CarlosHuamaniTec <carlos.huamani.pi@gmail.com>
2025-05-28 02:05:12 -0500

    Se movieron los archivos dentro de carpetas en la sección de microservicios

--- backend/microservices/sensor-data-service/sensor_data_service/src/main/java/com/vitalpaw/sensordata/model/SensorData.java
+++ backend/microservices/sensor-data-service/sensor_data_service/src/main/java/com/vitalpaw/sensordata/model/SensorData.java
@@ -5,13 +5,12 @@ import lombok.Setter;
 
 import java.time.LocalDateTime;
 
-// Esta clase no se guarda en la DB, solo se usa para procesar datos
 @Getter
 @Setter
 public class SensorData {
     private String petName;
     private Float temperature;
     private Integer heartRate;
-    private String movementState;
+    private MovementData movementData;
     private LocalDateTime timestamp;
 }
