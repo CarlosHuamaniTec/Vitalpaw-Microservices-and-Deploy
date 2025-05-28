c2cabac99335ac4f006210b04819a7530b9b6861
@CarlosHuamaniTec <carlos.huamani.pi@gmail.com>
2025-05-28 02:09:25 -0500

    Creación de modelos y servicios de la mascota

---
+++ backend/microservices/user-service/user_service/src/main/java/com/vitalpaw/userservice/repository/PetRepository.java
@@ -0,0 +1,10 @@
+package com.vitalpaw.userservice.repository;
+
+import com.vitalpaw.userservice.model.Pet;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface PetRepository extends JpaRepository<Pet, Long> {
+    List<Pet> findByOwnerId(Long ownerId);
+}
