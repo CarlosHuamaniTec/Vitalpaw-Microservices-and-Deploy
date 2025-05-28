a3c5fa6580a52653aef9b7ccde5097eb316c3748
@CarlosHuamaniTec <carlos.huamani.pi@gmail.com>
2025-05-28 00:05:37 -0500

    Reestructuración

--- backend/vitalpaw/backend/springboot/mainclient/src/main/java/com/vitalpaw/mainclient/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.vitalpaw.mainclient.repository;
-
-import com.vitalpaw.mainclient.model.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-    User findByEmail(String email);
-    User findByUsername(String username);
-}
